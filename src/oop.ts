8dcdf74d174022fcf6d5e7447596e4d02dba447d
RfxNaushad <rfxhridoy@gmail.com>
2024-05-15 21:25:46 +0600

    feat: access modifiers, instance of guard, In guard, key of guard

--- src/oop.ts
+++ src/oop.ts
@@ -94,3 +94,118 @@ teacher.makeClass('4')
 
 
 // type guard / type narrowing
+
+// key of guard
+
+type Alphanumeric = string | number;
+
+function add(param1: Alphanumeric, param2: Alphanumeric){ //this is called key of guard
+   if(param1 == "number" && param2 == "number"){
+    return param1 + param2
+   }else {
+     return param1.toString() + param2.toString()
+   }
+}
+
+console.log(add("1","2"));
+console.log(add(1,2));
+
+// IN guard. this works only in object
+type NormalTypePerson  = {
+    name: string
+}
+
+type AdminUserType = {
+    name: string
+    role: "admin"
+}
+
+function getUser(user: NormalTypePerson|AdminUserType): string{
+   if("role" in user){ //IN guard 
+    return (`I am an admin and my role is ${user.role}`);
+   } else {
+    return("I am a normal user");
+   }
+}
+
+const normalUser1: NormalTypePerson = {name: "mr kallu"}
+const adminUser: AdminUserType = {name: "mr ballu", role: "admin"}
+
+console.log(getUser(adminUser));
+
+
+// Instance of guard -- class and object guard kore
+
+class Animal3 {
+    name: string
+    species: string
+    constructor(name: string, species: string){
+        this.name = name
+        this.species = species
+    }
+
+    makeSound(){
+        console.log("I am making sound");
+    }
+}
+
+class Dog extends Animal3 {
+    constructor(name: string, species: string){
+        super(name, species)
+    }
+
+    makeBark(){
+        console.log("I am barking");
+    }
+}
+
+class Cat extends Animal3 {
+    constructor(name: string, species: string){
+        super(name, species)
+    }
+
+    makeMew(){
+        console.log("I am Mewing");
+    }
+}
+
+// 2nd way of using guard
+function isDog(animal: Animal3): animal is Dog {
+    return animal instanceof Dog
+}
+
+function getAnimal(animal: Animal3){
+   if(isDog(animal)){
+    animal.makeBark();
+   } else if(animal instanceof Cat){ // 1st way
+    animal.makeMew()
+   } else{
+    animal.makeSound()
+   }
+}
+
+const doggo = new Dog ("german bhai", "DOG") //instance of DOG Class
+const catto = new Cat ("persian bhai", "CAT")
+
+getAnimal(catto)
+
+
+// Access modifiers - public, private, protected
+class BankAccount {
+    id: number;
+    name: string;
+    private balance: number;
+
+    constructor(id: number, name: string, balance: number){
+        this.name = name,
+        this. id = id
+        this. balance = balance
+    }
+}
+
+// the difference between private and protected is that while private you can only use that properties in that associate class
+// protected means you can use that properties into other classes
+
+const myaccount = new BankAccount (444, 'persian', 20)
+
+console.log(myaccount)
